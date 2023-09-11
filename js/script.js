6b8c4081d79994e21596aadd443ac3c95ae838ea
Roberto Espeso <roberto.espeso96@gmail.com>
2023-09-11 03:28:26 -0300

    Actualizaciones varias
    - Agregado de clases y eliminación de código referencial en html.
    - creacion variables de colores para todo el proyecto.
    - actualizaciones de estilos en css.
    
    - Sistema de actualización de resultados e insertados en html.
    
    -Eliminación de alerts de referencia.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ var images = new Array(
   ["src/scissors-battle.png"]
 );
 
+var results = new Array(["Winner"], ["Loser"], ["Tie"]);
+
 let index = 0;
 let image = document.getElementById("cpuImage");
 const btnRock = document.getElementById("btnRock");
@@ -16,38 +18,40 @@ const btns = document.querySelectorAll(".buttons");
 const userResults = document.getElementById("userResults");
 const cpuResults = document.getElementById("cpuResults");
 let userImage = document.getElementById("userImage");
-let textWinsUSer = document.getElementById("textWinsUSer");
-let textWinsCpu = document.getElementById("textWinsCpu");
+let textAllWinsUser = document.getElementById("textAllWinsUser");
+let textAllWinsCpu = document.getElementById("textAllWinsCpu");
 
 let btnSelected;
 let userValue;
 let lengthImages = images.length;
-let winsUser = 0;
-let winsCpu = 0;
+let allWinsUser = 0;
+let allWinsCpu = 0;
 let min = 1;
 let max = 3;
 let cpuValue;
 let changeImages;
+let textUserResults = "";
+let textCpuResults = "";
+let iconUserResults;
+let iconCpuResults;
 
 const btnClicked = function (e) {
-  console.log(`El texto que tiene el boton clickeado es: ${this.innerText}`);
-  console.log(
-    `El contenido html que tiene el boton clickeado es: ${this.innerHTML}`
-  );
+  // console.log(`El texto que tiene el boton clickeado es: ${this.innerText}`);
+  // console.log(`El contenido html que tiene el boton clickeado es: ${this.innerHTML}`);
   let userTextSelection = this.innerHTML;
   if (userTextSelection.includes("Rock")) {
-    userImage.attributes.src.value = "src/rock-battle.png";
+    userImage.attributes.src.value = images[1];
     userValue = 1;
   } else if (userTextSelection.includes("Paper")) {
-    userImage.attributes.src.value = "src/paper-battle.png";
+    userImage.attributes.src.value = images[2];
     userValue = 2;
   } else {
-    userImage.attributes.src.value = "src/scissors-battle.png";
+    userImage.attributes.src.value = images[3];
     userValue = 3;
   }
   this.classList.add("btnChosenUser");
   btnSelected = this.classList;
-  console.log(btnSelected);
+  //console.log(btnSelected);
   return userValue;
 };
 
@@ -87,8 +91,8 @@ function ppt() {
     clearInterval(changeImages);
     cpuValue = Math.floor(Math.random() * (max - min + 1) + min);
     image.src = images[cpuValue];
-    console.log(`Este es el valor del cpu ${images[cpuValue]}`);
-    console.log(cpuValue);
+    //console.log(`Este es el valor del cpu ${images[cpuValue]}`);
+    //console.log(cpuValue);
     battle();
     enableBtns();
     removeBtnSelected();
@@ -99,48 +103,75 @@ function battle() {
   switch (true) {
     case userValue === 1 && cpuValue === 3:
       //piedra vs tijera
-      winsUser++;
+      allWinsUser++;
+      textUserResults = results[0];
+      textCpuResults = results[1];
+      iconUserResults = images[userValue];
+      iconCpuResults = images[cpuValue];
       writeResults(userValue, cpuValue);
       break;
     case userValue === 2 && cpuValue === 1:
       //papel vs piedra
-      winsUser++;
+      allWinsUser++;
+      textUserResults = results[0];
+      textCpuResults = results[1];
+      iconUserResults = images[userValue];
+      iconCpuResults = images[cpuValue];
       writeResults(userValue, cpuValue);
-      alert("Ganaste papel vs piedra");
       break;
     case userValue === 3 && cpuValue === 2:
       // tijera vs papel
-      winsUser++;
+      allWinsUser++;
+      textUserResults = results[0];
+      textCpuResults = results[1];
+      iconUserResults = images[userValue];
+      iconCpuResults = images[cpuValue];
       writeResults(userValue, cpuValue);
-      alert("Ganaste tijera vs apapel");
       break;
 
     case cpuValue === 1 && userValue === 3:
       //piedra vs tijera
-      winsCpu++;
+      allWinsCpu++;
+      textUserResults = results[1];
+      textCpuResults = results[0];
+      iconUserResults = images[userValue];
+      iconCpuResults = images[cpuValue];
       writeResults(userValue, cpuValue);
-      alert("perdistes piedra vs tijera");
       break;
     case cpuValue === 2 && userValue === 1:
       //papel vs piedra
-      winsCpu++;
+      allWinsCpu++;
+      textUserResults = results[1];
+      textCpuResults = results[0];
+      iconUserResults = images[userValue];
+      iconCpuResults = images[cpuValue];
       writeResults(userValue, cpuValue);
-      alert("perdistes papel vs piedra");
       break;
     case cpuValue === 3 && userValue === 2:
       // tijera vs papel
-      winsCpu++;
+      allWinsCpu++;
+      textUserResults = results[1];
+      textCpuResults = results[0];
+      iconUserResults = images[userValue];
+      iconCpuResults = images[cpuValue];
       writeResults(userValue, cpuValue);
-      alert("perdistes tijera vs apapel");
       break;
     case cpuValue === userValue:
+      textUserResults = results[2];
+      textCpuResults = results[2];
+      iconUserResults = images[userValue];
+      iconCpuResults = images[cpuValue];
+      writeResults(userValue, cpuValue);
       // Empataron
-      alert("Empataron");
       break;
   }
 }
 
-function writeResults(userValue, cpuValue) {
-  textWinsUSer.innerText = winsUser;
-  textWinsCpu.innerText = winsCpu;
+function writeResults() {
+  textAllWinsUser.innerText = allWinsUser;
+  textAllWinsCpu.innerText = allWinsCpu;
+  userResults.innerHTML += `<div class="player-results"> <p>${textUserResults}</p>
+  <img class="icon-results" src="${iconUserResults}" alt="" /></div>`;
+  cpuResults.innerHTML += `<div class="cpu-results"> <p>${textCpuResults}</p>
+  <img class="icon-results" src="${iconCpuResults}" alt="" /></div>`;
 }
